91c18db94a8a10c4b3e51a55050a37508bf31fe4
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-04-07 17:18:51 +0200

    push

--- simclr/modules/logistic_regression.py
+++ simclr/modules/logistic_regression.py
@@ -4,8 +4,14 @@ import torch.nn as nn
 class LogisticRegression(nn.Module):
     def __init__(self, n_features, n_classes):
         super(LogisticRegression, self).__init__()
-
-        self.model = nn.Linear(n_features, n_classes)
+        self.linear = nn.Sequential(
+            nn.Linear(n_features, n_features),
+            nn.ReLU(),
+            nn.Linear(n_features, n_features),
+            nn.ReLU(),
+            nn.Linear(n_features, 128),
+            nn.Linear(128, n_classes),
+        )
 
     def forward(self, x):
-        return self.model(x)
+        return self.linear(x)
